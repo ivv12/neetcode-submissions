ab8c47f4f8b09e8b9e87464208ba7cd3ab095bda
Venya Velmurugan <velven1206@gmail.com>
2026-05-20 01:32:46 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for (int i=0;i<9;i++)
+        {
+            for (int j=0;j<9;j++)
+            {
+                if (board[i][j]=='.')
+                {
+                    continue;
+                }
+
+                else
+                {
+                    int num=board[i][j]-'1';
+                    int x=(i/3)*3+(j/3);
+                    if (rows[i][num] || cols[j][num] || boxes[x][num])
+                    {
+                        return false;
+                    }
+
+                    rows[i][num]=cols[j][num]=boxes[x][num]=true;
+
+                }
+
+            }
+        }
+
+        return true;
+
+    }
+};
