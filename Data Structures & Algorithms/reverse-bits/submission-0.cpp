c1591cedb93f7b1c06d8f118bbf98325d7d6ec3a
Venya Velmurugan <velven1206@gmail.com>
2026-06-03 14:31:35 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        string s = bitset<32>(n).to_string();
+
+        reverse(s.begin(), s.end());
+
+        return static_cast<uint32_t>(stoul(s, nullptr, 2));
+    }
+};
