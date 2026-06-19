7714db2c9204ab8dc37b4139c95b1a75c8b7b8ae
Venya Velmurugan <velven1206@gmail.com>
2026-06-20 01:13:15 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> seen;
+        for (int i=0;i<nums.size();i++)
+        {
+            if (seen.find(nums[i])!=seen.end())
+            {
+                return true;
+            }
+
+            seen[nums[i]]=1;
+        }
+
+        return false;
+    }
+};
