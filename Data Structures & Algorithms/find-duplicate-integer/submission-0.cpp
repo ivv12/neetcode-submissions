277f9b0e0fd95242bdae63e27925605e9e9aaeb9
Venya Velmurugan <velven1206@gmail.com>
2026-06-21 23:35:57 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> freq(nums.size()+1,0);
+
+        for (int i=0;i<nums.size();i++)
+        {
+            if (freq[nums[i]]>0)
+            {
+                return nums[i];
+            }
+
+            freq[nums[i]]++;
+        }
+    }
+};
