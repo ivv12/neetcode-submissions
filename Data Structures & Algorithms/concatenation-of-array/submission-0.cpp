520020ec39c54656ac23eef16ee536b299c82c84
Venya Velmurugan <velven1206@gmail.com>
2026-06-26 22:04:28 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> res(2*nums.size(),0);
+
+        for (int i=0;i<nums.size();i++)
+        {
+            res[i]=res[nums.size()+i]=nums[i];
+        }
+
+        return res;
+    }
+};
