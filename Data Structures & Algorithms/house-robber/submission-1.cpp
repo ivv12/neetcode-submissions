4d0ac8fd807548b259086ed838ca80f739fe4d1a
Venya Velmurugan <velven1206@gmail.com>
2026-07-27 01:28:00 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,0);
+
+        for (int i=0;i<nums.size();i++)
+        {
+            if (i==0)
+            {
+                dp[i]=nums[i];
+            }
+
+            else if (i==1)
+            {
+                dp[i]=max(nums[0],nums[1]);
+            }
+
+            else
+            {
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+            }
+        }
+
+        return dp[nums.size()-1];
+    }
+};
