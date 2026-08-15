2a7aa130ece6dba982520be0cccefb6e554d1e51
Venya Velmurugan <velven1206@gmail.com>
2026-08-15 22:22:08 +0530

    Add: maximum-ascending-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/maximum-ascending-subarray-sum/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1]) {
+                currentSum += nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+            maxSum =max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
